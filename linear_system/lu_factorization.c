b71e05bb993b23b32a2810e2898103707596ed3c
Guilherme M. Lopes <guilehrme.glopes@mercadolivre.com>
2022-07-16 11:42:30 -0300

    add lu factorization interface

---
+++ linear_system/lu_factorization.c
@@ -0,0 +1,14 @@
+#include "lu_factorization.h"
+
+
+RealNumber **generateMatrixL(RealNumber **A, RealNumber **B, RealNumber **U, int n) {
+    // TODO
+}
+
+RealNumber **InvertMatrix(RealNumber **A, int n) {
+    // TODO
+}
+
+RealNumber CalculateResidue(RealNumber **A, RealNumber **invertedA, int n) {
+    // TODO
+}
---
+++ linear_system/lu_factorization.h
@@ -0,0 +1,24 @@
+#ifndef LU_FACTORIZATION_HEADER
+#define LU_FACTORIZATION_HEADER
+
+#include "../common/common.h"
+
+// invertMatrix returns the inverted matrix of the matrix A of size n
+// 1) Get L and U by solving -> L = generateMatrixL(A, B, U, n);
+// 2) Get the y arrays by solving -> Ly = b;
+// 3) Get the inverted matrix x by solving -> Ux = y for each y.
+// 4) Returns x.
+RealNumber **InvertMatrix(RealNumber **A, int n);
+
+// generateMatrixL returns the multiplier's matrix L of a matrix A of size n
+// using Gauss Elimination with partial pivoting. The partial pivoting should
+// also replace lines in the B matrix (identity matrix).
+// Also stores the echelon form of A inside U.
+RealNumber **generateMatrixL(RealNumber **A, RealNumber **B, RealNumber **U, int n);
+
+// CalculateResidue returns the L2 Norm of the residue for the current result.
+// R = I - A * A^-1
+// L2Norm = sqrt(sum(R[i][j]^2)), ∀ 1 <= i, j <= n
+RealNumber CalculateResidue(RealNumber **A, RealNumber **invertedA, int n);
+
+#endif
